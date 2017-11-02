b1619d077cfa3f8dbee9c537875b46888768f813
Chase Paules <cpaules@gmail.com>
2017-11-02 19:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-function getIssues() {
+function getIssues(){
+  const token= getToken()
+  fetch(`https://api.github.com/repos/cpaules/javascript-fetch-lab/issues`, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => showIssues(res));
 }
 
 function showIssues(json) {
+  let theIssues = ""
+  for (var i = 0; i < json.length; i++) {
+    theIssues = theIssues+` title: ${json[i].title} body: ${json[i].body}`
+  }
+  $("#issues").append(theIssues)
 }
 
+
 function createIssue() {
+  owner = document.getElementById("results").innerText
+  theTitle= document. getElementById("title").value
+  theBody = document.getElementById("body").value
+  const postData = {title: theTitle, body : theBody}
+  const repo = 'cpaules/javascript-fetch-lab'
+  const token= getToken()
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => getIssues(res));
 }
 
 function showResults(json) {
